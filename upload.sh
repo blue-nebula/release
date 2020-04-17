489beef0e4d7bf39161470d4611ba34acd38a845
TheAssassin <theassassin@assassinate-you.net>
2020-04-17 15:30:34 +0200

    AppImage builds, mk. 1

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ "$TRAVIS_CI" == "" ]; then
+    echo "Error: this script is supposed to run on Travis CI"
+    exit 1
+fi
+
+# if not building for master, do not upload to GitHub
+if [ "$TRAVIS_BRANCH" != "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" != "master" ]; then export TRAVIS_EVENT_TYPE=pull_request; fi
+
+wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+
+bash upload.sh Red_Eclipse_Legacy*.AppImage
