b34c5a9d9d72c3ad1af9cbbb33add049e27afe54
TheAssassin <theassassin@assassinate-you.net>
2020-04-17 16:25:52 +0200

    Try to fix weird -c related error

--- upload.sh
+++ upload.sh
@@ -8,6 +8,6 @@ fi
 # if not building for master, do not upload to GitHub
 if [ "$TRAVIS_BRANCH" != "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" != "master" ]; then export TRAVIS_EVENT_TYPE=pull_request; fi
 
-wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+wget https://github.com/probonopd/uploadtool/raw/master/upload.sh
 
 bash upload.sh Red_Eclipse_Legacy*.AppImage
