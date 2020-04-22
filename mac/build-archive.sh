1b2b26e8861882951bb9ed21ba98563bfadf11d0
TheAssassin <theassassin@assassinate-you.net>
2020-04-22 22:13:24 +0200

    macOS x86_64 zipball, mk. 1

---
+++ mac/build-archive.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+set -e
+set -x
+
+case "$ARCH" in
+    x86_64|amd64|win64)
+        export ARCH="x86_64"
+        ;;
+    i386|i586|i686|win32)
+        export ARCH="i686"
+        ;;
+    *)
+        echo "Error: \$ARCH unset, please export ARCH=... (e.g., i686, x86_64)"
+        exit 1
+        ;;
+esac
+
+# RAM disk is not available on mac, therefore we simply use a temporary directory
+BUILD_DIR=$(mktemp -d relegacy-build-XXXXXX)
+
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+
+trap cleanup EXIT
+
+# store repo root as variable
+REPO_ROOT=$(readlink -f $(dirname $(dirname "$0")))
+OLD_CWD=$(readlink -f .)
+
+pushd "$BUILD_DIR"
+
+git clone --recursive https://github.com/redeclipse-legacy/base.git
+
+cd base
+
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+
+make preinstall -j$(nprocs)
+
+cpack -G ZIP -V
+
+mv *.zip "$OLD_CWD"
