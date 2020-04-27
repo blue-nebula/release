0636065bea5cbda98644423600dac9b539beb975
TheAssassin <theassassin@assassinate-you.net>
2020-04-27 02:43:19 +0200

    Bundle library dependencies, too, mk. 1

--- mac/build-archive.sh
+++ mac/build-archive.sh
@@ -16,8 +16,18 @@ case "$ARCH" in
         ;;
 esac
 
-# RAM disk is not available on mac, therefore we simply use a temporary directory
-BUILD_DIR=$(mktemp -d relegacy-build-XXXXXX)
+# use RAM disk if possible
+if [ "$CI" == "" ] && [ -d /dev/shm ]; then
+    TEMP_BASE=/dev/shm
+fi
+
+# use RAM disk if possible (e.g., while cross-compiling)
+# RAM disk is not available on mac, therefore we use the system default there
+if [ ! -z "$TEMP_BASE" ]; then
+    BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" relegacy-build-XXXXXX)
+else
+    BUILD_DIR=$(mktemp -d relegacy-build-XXXXXX)
+fi
 
 cleanup () {
     if [ -d "$BUILD_DIR" ]; then
@@ -33,6 +43,47 @@ OLD_CWD=$(realpath .)
 
 pushd "$BUILD_DIR"
 
+# first, we need to build macdylibbundler
+git clone https://github.com/auriamg/macdylibbundler
+pushd macdylibbundler
+
+# we have to apply a minor patch: we need to be able to set the otool executable from an env var
+git apply <<\EOF
+diff --git a/src/DylibBundler.cpp b/src/DylibBundler.cpp
+index e7bf46b..6e359bf 100644
+--- a/src/DylibBundler.cpp
++++ b/src/DylibBundler.cpp
+@@ -71,7 +71,7 @@ void collectRpaths(const std::string& filename)
+         return;
+     }
+ 
+-    std::string cmd = "otool -l " + filename;
++    std::string cmd = std::string(getenv("OTOOL")) + " -l " + filename;
+     std::string output = system_get_output(cmd);
+ 
+     std::vector<std::string> lc_lines;
+@@ -199,7 +199,7 @@ void addDependency(std::string path, std::string filename)
+ void collectDependencies(std::string filename, std::vector<std::string>& lines)
+ {
+     // execute "otool -l" on the given file and collect the command's output
+-    std::string cmd = "otool -l " + filename;
++    std::string cmd = std::string(getenv("OTOOL")) + " -l " + filename;
+     std::string output = system_get_output(cmd);
+ 
+     if(output.find("can't open file")!=std::string::npos or output.find("No such file")!=std::string::npos or output.size()<1)
+EOF
+
+make -j7
+
+# it just creates the binary here, so let's just add it to $PATH to be able to access it conveniently
+export PATH="$(realpath .):$PATH"
+
+popd
+
+# after having applied that patch above, we need to ensure $OTOOL is set or the whole thing will segfault
+export OTOOL="${OTOOL:-otool}"
+
+# now let's build the binaries and install them into the usual FHS-style directory tree
 git clone --recursive https://github.com/redeclipse-legacy/base.git
 
 cd base
@@ -40,10 +91,34 @@ cd base
 mkdir build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+# allow overwriting for cross compilation
+export CMAKE="${CMAKE:-cmake}"
 
-make preinstall -j$(nprocs)
+if type nproc &>/dev/null; then
+    procs=$(nproc --ignore=1)
+else
+    # macos doesn't have nproc, and nprocs does not support ignore
+    procs=$(nprocs)
+fi
 
-cpack -G ZIP -V
+# we do not need any prefix in our zip archive, so we just write /
+"$CMAKE" .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/
 
+make preinstall -j"$procs"
+
+# install everything into a temporary FHS-style tree which we will later zip manually after having collected the deps
+make install DESTDIR=out &>install.log
+
+# now, we can use macdylibbundler to collect the deps
+# NOTE: THIS STEP DOES ONLY WORK ON MACOS PROPERLY
+# we could also patch the tool more (and provide search paths via -s for the osxcross libs), but it's easier to just run it on macOS
+dylibbundler -od -b -x out/bin/redeclipse-legacy_osx -d out/lib/
+
+# copy the license files from the bundled Windows libs dir... to be on the safe side
+cp ../src/bundled-libs/"${ARCH}"-w64-mingw32/lib/LICENSE* out/lib/
+
+# let's build the final zip archive
+zip -r redeclipse-legacy-$(git describe --tags)-macos-"$ARCH".zip out/* &>zip.log
+
+# move the build product back into the filesystem
 mv *.zip "$OLD_CWD"
